17a1aee3146081e4e5fe2db3702a3d5035eb83b5
ahzamaf <165181358+ahzamaf@users.noreply.github.com>
2024-04-05 19:57:04 +0500

    Add files via upload

---
+++ no_users[ex#30].js
@@ -0,0 +1,9 @@
+"use strict";
+let userNames = ['maaz', 'ali', 'ahzam', 'daniyal', 'ather'];
+if (userNames.length === 0) {
+    console.log('we need to find some users!');
+}
+else {
+    userNames = [];
+    console.log('all user name have been removed' + userNames.length);
+}
